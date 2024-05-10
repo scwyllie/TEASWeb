577b2c80c04365f7fc7963f186641779a12f32b8
Sierra Wyllie <sierrawyllie@Aeglos.local>
2024-05-09 22:40:51 -0400

    upload

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const {src, dest, watch, series} = require('gulp');
+const fileinclude = require('gulp-file-include')
+const terser = require('gulp-terser')
+const browserSync = require('browser-sync').create();
+
+
+function file_include_task() {
+	return src(['index.html'])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(dest('./'));
+}
+
+
+function browserSyncReload(cb) {
+  browserSync.reload();
+  cb();
+}
+
+function watcher(cb) {
+	watch('index.html', browserSyncReload);
+}
+
+function browser_sync(cb) {
+	var browserSyncOptions = {
+		browser: "google chrome",
+    	server: "./",
+    	notify: false
+	};
+	browserSync.init(browserSyncOptions);
+	cb();
+}
+
+exports.default = series(
+	file_include_task,
+	browser_sync,
+	watcher
+)
+
